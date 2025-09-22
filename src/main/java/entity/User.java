efb2c0bf1d8aea6c9364b9a921c864ac7d28d92f
anbui <anbui1435@gmail.com>
2025-09-22 15:56:22 +0700

    an duc lam

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,49 @@
+package entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    // getters and setters
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public User(String password, String username, Long id) {
+        this.password = password;
+        this.username = username;
+        this.id = id;
+    }
+    public User() {
+    }
+}
