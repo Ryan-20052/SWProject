37567762d5a40dc5ea6a15521797dcc3da0bed12
dmd955 <doiminhdat955@gmail.com>
2025-09-27 21:29:17 +0700

    b

---
+++ src/main/java/anbd/he191271/controller/ManagerController.java
@@ -0,0 +1,22 @@
+package anbd.he191271.controller;
+
+import anbd.he191271.entity.Product;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/manage")
+public class ManagerController {
+
+@GetMapping("/getProduct")
+    public ResponseEntity<ArrayList<Product>> getProduct(){
+
+}
+
+}
